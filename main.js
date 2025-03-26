de19570358e5203d55e24d10afb2c1365671f706
Arafat Gamzawe <arafat.trans@gmail.com>
2025-03-26 03:07:13 +0300

    // content: i got a new idea and make it todo list

--- main.js
+++ main.js
@@ -1,27 +1,18 @@
+// Helper to create unique IDs
+function createUniqueId() {
+  return 'task-' + Math.random().toString(36).substring(2, 11);
+}
 
-  // Generate id for nodes
-  function generateId() {
-    return `vnode-${Math.random().toString(36).substr(2, 9)}`;
-  }
+// Create virtual DOM nodes
+function createNode(type, attributes = {}, children = [], text = '') {
+  return {
+    id: createUniqueId(),
+    type: type,
+    attributes,
+    children,
+    text
+  };
+}
 
-  // Function to create a VNode as a plain object
-  function createVNode(tag, props = {}, children = [], textContent = "") {
-    return {
-      id: generateId(),
-      tag,        // e.g., 'div', 'span', 'h1'
-      props,      // e.g., { class: 'container', style: 'color: red' }
-      children,   // Array of child VNodes
-      textContent // Text inside the node if applicable
-    };
-  }
-    // Create the initial Virtual DOM Structure
-    let virtualDOM = createVNode("div", { class: "container" }, [
-      createVNode("h1", {}, [], "Welcome to Virtual DOM"),
-      createVNode("p", {}, [], "This is a paragraph inside the virtual DOM."),
-      createVNode("ul", {}, [
-        createVNode("li", {}, [], "Item 1"),
-        createVNode("li", {}, [], "Item 2"),
-        createVNode("li", {}, [], "Item 3")
-      ])
-    ]);
-  
+// Initial Todo List virtual DOM
+let mainDOM = createNode('div', { class: 'list-group' }, []);
